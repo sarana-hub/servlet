bd0c8c862a51c6c478372e7a17827312e7d9984d
nayeon-jo <nyc0516@naver.com>
2022-01-17 19:07:09 +0900

    Initial commit

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -13,9 +14,20 @@ public class MyView {
         this.viewPath =viewPath;
     }
 
-    public void render(HttpServletRequest request, HttpServletResponse response)
+    public void render(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request, response);
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException{
+        modelToRequestAttribute(model, req);
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest req) {
+        model.forEach((key, value)-> req.setAttribute(key,value));
     }
 }
