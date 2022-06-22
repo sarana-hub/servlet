381da6d131ad57cb2149e8c04bd86958c73b5e0f
ny <nyc0516@naver.com>
2022-06-22 19:44:52 +0900

    서블릿 등록하기

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -11,14 +11,15 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         System.out.println("HelloServlet.service");
-        System.out.println("req = " + req);
-        System.out.println("resp = " + resp);
+        System.out.println("request = " + req);
+        System.out.println("response = " + resp);
 
         String username = req.getParameter("username");
         System.out.println("username = " + username);
 
-        resp.setContentType("text/plain");
+        resp.setContentType("text/plain");  //단순 문자
         resp.setCharacterEncoding("utf-8");
         resp.getWriter().write("hello "+username);
     }
