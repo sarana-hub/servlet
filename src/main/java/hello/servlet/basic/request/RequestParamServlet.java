acf82a68657d2e17c76961f7d13c7debd9581f8d
ny <nyc0516@naver.com>
2022-06-23 14:51:31 +0900

    HTTP요청데이터 -GET 쿼리 파라미터 (쿼리 파라미터 조회 메소드 실습)

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 /**
  * 1. 파라미터 전송 기능
  * http://localhost:8080/request-param?username=hello&age=20
- * <p>
+ *
  * 2. 동일한 파라미터 전송 가능
  * http://localhost:8080/request-param?username=hello&username=kim&age=20
  */
@@ -20,30 +20,24 @@ public class RequestParamServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");
-
         req.getParameterNames().asIterator()
-                .forEachRemaining(paramName -> System.out.println(paramName +
-                        "=" + req.getParameter(paramName)));
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + req.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
         System.out.println();
 
 
         System.out.println("[단일 파라미터 조회]");
         String username = req.getParameter("username");
-        System.out.println("username = " + username);
-
+        System.out.println("username = " + username);    //hello
         String age = req.getParameter("age");
-        System.out.println("age = " + age);
+        System.out.println("age = " + age);     //20
         System.out.println();
 
 
         System.out.println("[이름이 같은 복수 파라미터 조회]");
-        System.out.println("request.getParameterValues(username)");
         String[] usernames = req.getParameterValues("username");
         for (String name : usernames) {
-            System.out.println("username=" + name);
+            System.out.println("username=" + name);  //hello  //kim
         }
-
-        resp.getWriter().write("ok"); //
     }
 }
