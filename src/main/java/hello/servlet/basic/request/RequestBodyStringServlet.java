f40bd2a0ad209988ecfbbf1c5d0d5831a15b88ef
nayeon-jo <nyc0516@naver.com>
2023-07-20 15:50:47 +0900

    HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -13,14 +13,17 @@ import java.nio.charset.StandardCharsets;
 
 @WebServlet(name="requestBodyStringServlet", urlPatterns = "/request-body-string")
 public class RequestBodyStringServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         //HTTP메시지바디의 데이터는 inputStream을 사용해서 직접 읽을 수 있다
         ServletInputStream inputStream=req.getInputStream();
-        //inputStream은 byte코드를 반환하므로, 문자로 보려면 Charset을 지정해주어야함
+        //inputStream은 byte를 반환하므로, 문자로 변환하려면 Charset을 지정해주어야 한다
         String messageBody= StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
-        System.out.println("messageBody = " + messageBody);     //hello
+        System.out.println("messageBody = " + messageBody);     //hello!
+
         resp.getWriter().write("ok");
 
     }
