7eb8e82a9ae99f45d617f2e1a699a5c47f68c803
nayeon-jo <nyc0516@naver.com>
2023-07-21 14:00:13 +0900

    HTTP 응답 데이터 - HTML

--- src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -10,17 +10,19 @@ import java.io.PrintWriter;
 
 @WebServlet(name="responseHtmlServlet", urlPatterns = "/response-html")
 public class ResponseHtmlServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         //Content-Type: text/html;charset=utf-8
-        //HTTP 응답으로 HTML을 반환할 때는 content-type을 text/html 로 지정해야 한다
+        //HTTP 응답으로 HTML을 반환할 땐, content-type을 text/html로 지정
         resp.setContentType("text/html");
         resp.setCharacterEncoding("utf-8");
 
         PrintWriter writer = resp.getWriter();
         writer.println("<html>");
         writer.println("<body>");
-        writer.println("    <div>안녕?</div>");
+        writer.println("    <div>안녕~?</div>");
         writer.println("</body>");
         writer.println("</html>");
     }
