d64b5cef0a8df0fc2d420a3990a8c112f883d9f7
nayeon-jo <nyc0516@naver.com>
2023-08-05 21:49:13 +0900

    핸들러 매핑과 핸들러 어댑터

--- src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
+++ src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//HttpRequestHandler 인터페이스: 서블릿과 가장 유사한 형태의 핸들러
 
 @Component("/springmvc/request-handler")
 public class MyHttpRequestHandler implements HttpRequestHandler {
+
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("MyHttpRequestHandler.handleRequest");
-
     }
+
 }
--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -7,15 +7,16 @@ import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//Controller 인터페이스: 과거 버전 스프링 컨트롤러
 
-@Component("/springmvc/old-controller")    //이 컨트롤러는 /springmvc/old-controller 라는 이름의 스프링 빈으로 등록되었다
+@Component("/springmvc/old-controller")
 public class OldController implements Controller {
+
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
 
         return new ModelAndView("new-form");
-        //뷰 리졸버 등록해야함-> application.properties에 코드 추가
     }
+
 }
+//뷰 리졸버 등록해야함-> application.properties에 코드 추가
