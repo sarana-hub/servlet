fdf4c66605ef07e1e0afca28025a338d393adc76
nayeon-jo <nyc0516@naver.com>
2023-08-06 21:23:36 +0900

    뷰 리졸버

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -14,9 +14,7 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-
         return new ModelAndView("new-form");
     }
 
 }
-//뷰 리졸버 등록해야함-> application.properties에 코드 추가
