72ec8e9b1428cf49a15934910640d506cb0787e0
nayeon-jo <nyc0516@naver.com>
2023-08-08 13:41:18 +0900

    스프링 MVC 컨트롤러 통합

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -10,40 +10,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-/**
- * 클래스 단위 -> 메서드 단위
- * @RequestMapping 클래스 레벨과 메서드 레벨 조합
- */
+
 @Controller
-@RequestMapping("/springmvc/v2/members")    // 컨트롤러 클래스를 통합,조합
+@RequestMapping("/springmvc/v2/members")    //메서드 레벨과 조합
 public class SpringMemberControllerV2 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form") // /springmvc/v2/members/new-form
+
+    @RequestMapping("/new-form")    //조합 결과: /springmvc/v2/members/new-form
     public ModelAndView newForm(){
         return new ModelAndView("new-form");
     }
 
+
     @RequestMapping("/save")    // /springmvc/v2/members/save
-    public ModelAndView save(HttpServletRequest request, HttpServletResponse response){
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+    public ModelAndView save(HttpServletRequest req, HttpServletResponse resp){
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
 
         Member member = new Member(username, age);
         memberRepository.save(member);
 
         ModelAndView mv = new ModelAndView("save-result");
-        mv.addObject("member", member);
+        mv.addObject("member", member); //Model 데이터 추가
         return mv;
     }
+
 
     @RequestMapping     // /springmvc/v2/members
     public ModelAndView members() {
         List<Member> members = memberRepository.findAll();
-
         ModelAndView mv = new ModelAndView("members");
         mv.addObject("members", members);
-
         return mv;
     }
+
 }
